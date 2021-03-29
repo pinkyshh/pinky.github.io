a3c2b7eed170587961bfb490fd31e90b5e5a6403
pinkyshh <u6135205@au.edu>
2021-03-29 18:06:31 +0700

    Final Project

---
+++ final-project/src/Components/Products.js
@@ -0,0 +1,32 @@
+import React, { useContext } from 'react'
+import { ProductsContext } from '../Global/ProductsContext'
+import { CartContext } from '../Global/CartContext'
+export const Products = () => {
+
+    const { products } = useContext(ProductsContext);
+
+    const { dispatch } = useContext(CartContext);
+
+    return (
+        <>
+            {products.length !== 0}
+            <div className='products-container'>
+                {products.length === 0 && <div>slow internet...no products to display</div>}
+                {products.map(product => (
+                    <div className='product-card' key={product.ProductType}>
+                        <div className='product-img'>
+                            <img src={product.ProductImg} alt="not found" />
+                        </div>
+                        <div className='product-name'>
+                            {product.ProductName}
+                        </div>
+                        <div className='product-price'>
+                            ฿ {product.ProductPrice}.00
+                    </div>
+                        <button className='addcart-btn' onClick={() => dispatch({ type: 'ADD_TO_CART', id: product.ProductID, product })}>ADD TO CART</button>
+                    </div>
+                ))}
+            </div>
+        </>
+    )
+}
